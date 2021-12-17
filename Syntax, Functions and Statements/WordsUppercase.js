deab3123eace6cb5b6f6f8572dbbd74ef076ada5
Evgeni Genov <43596303+Evgeni-Genov@users.noreply.github.com>
2021-12-17 12:55:07 +0200

    Add files via upload

---
+++ Syntax, Functions and Statements/WordsUppercase.js
@@ -0,0 +1,10 @@
+function convert(input) {
+    let result = input.toUpperCase()
+        .match(/\w+/g)
+        .join(', ');
+
+    console.log(result);
+}
+
+convert('Hi, how are you?');
+convert('Functions in JS can be nested, i.e. hold other functions');
