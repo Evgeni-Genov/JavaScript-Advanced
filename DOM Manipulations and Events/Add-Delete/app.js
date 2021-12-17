e377b5123d53b9196b35828b8519018912a299e9
Evgeni Genov <43596303+Evgeni-Genov@users.noreply.github.com>
2021-12-17 12:32:49 +0200

    Add files via upload

---
+++ DOM Manipulations and Events/Add-Delete/app.js
@@ -0,0 +1,21 @@
+function addItem() {
+    let text = document.getElementById('newText');
+    let newLi = document.createElement('li');
+
+    newLi.textContent = text.value;
+
+    const deleteButton = document.createElement('a');
+
+    deleteButton.textContent = '[Delete]';
+    deleteButton.href = '#';
+    deleteButton.addEventListener('click', (ev) => {
+        ev.target.parentNode.remove();
+    });
+
+    newLi.appendChild(deleteButton);
+
+    document.getElementById('items').appendChild(newLi);
+
+    text.value = '';
+
+}
---
+++ DOM Manipulations and Events/List-Of-Items/app.js
@@ -0,0 +1,11 @@
+function addItem() {
+    const ul = document.getElementById('items');
+    const text = document.getElementById('newItemText').value;
+    const newLi = document.createElement('li');
+
+    newLi.textContent = text;
+
+    ul.appendChild(newLi);
+    
+    document.getElementById('newItemText').value = '';
+}
