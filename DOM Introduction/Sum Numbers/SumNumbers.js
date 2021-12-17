07239438cc6a8cee62ba82ec8dcfa9dec09f8854
Evgeni Genov <43596303+Evgeni-Genov@users.noreply.github.com>
2021-12-17 12:26:31 +0200

    Add files via upload

---
+++ DOM Introduction/Sum Numbers/SumNumbers.js
@@ -0,0 +1,8 @@
+function calc() {
+    const firstNum = Number(document.querySelector('#num1').value);
+    const secondNum = Number(document.querySelector('#num2').value);
+
+    let res = document.querySelector('#sum');
+    
+    res.value = firstNum + secondNum;
+}
